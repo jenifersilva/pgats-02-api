6eb1740a0841da2bdf4461bc6c751b8aff9b2ca6
Jenifer Silva <jssilva423@gmail.com>
2025-08-19 16:01:51 -0300

    add user mocked tests

---
+++ test/controller/userController.test.js
@@ -0,0 +1,43 @@
+const request = require("supertest");
+const sinon = require("sinon");
+const { expect } = require("chai");
+const app = require("../../app.js");
+const userService = require("../../service/userService.js");
+
+describe("User controller com service mocked", () => {
+  describe("POST /register", () => {
+    it("Quando informo usuário já existente recebo status code 400", async () => {
+      const userServiceMock = sinon.stub(userService, "registerUser");
+      userServiceMock.throws(new Error("Usuário já existe"));
+
+      const resposta = await request(app)
+        .post("/register")
+        .send({
+          username: "string",
+          password: "string",
+          favorecidos: ["string"],
+        });
+      expect(resposta.status).to.equal(400);
+    });
+  });
+
+  describe("POST /login", () => {
+    it("Quando informo credenciais inválidas recebo status code 401", async () => {
+      const userServiceMock = sinon.stub(userService, "authenticateUser");
+      userServiceMock.throws(new Error('Credenciais inválidas'));
+
+      const resposta = await request(app)
+        .post("/login")
+        .send({
+            username: "string",
+            password: "string"
+        });
+      expect(resposta.status).to.equal(401);
+    });
+  });
+
+  afterEach(() => {
+    // Reseta os mocks, sem esse reset o sinon vai persistir o mock para outros testes
+    sinon.restore();
+  });
+});
